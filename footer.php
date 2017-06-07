b8518bedd5f55de7b76b49d4a07c4a9ab2be56e3
Zach <Zach>
2017-06-07 15:22:16 -0600

    Added footer content and stylings. Made a few changes to other page stylings that were off.

--- footer.php
+++ footer.php
@@ -15,9 +15,26 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'RapBetter-Theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'RapBetter-Theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'RapBetter-Theme' ), 'RapBetter-Theme', '<a href="https://automattic.com/" rel="designer">Zach Slagowski</a>' ); ?>
+			<div class="container">
+				<div class="row">
+					<div class="rb-footer-icons col-md-6 text-center">
+						<?php
+
+						$query = new WP_Query( array( 'post_type' => 'rb_footer_social' ) );
+
+						if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
+
+						?>
+
+						<a href="<?php the_field('rb-sm-footer-link'); ?>" target="__blank"><i class="fa <?php the_field('rb-footer-icon-class'); ?>" aria-hidden="true"></i></a>
+
+						<?php endwhile; endif; wp_reset_postdata; ?>
+					</div>
+					<div class="col-md-6 rb-copyright text-center">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class="rb-footer-logo" src="<?php header_image(); ?>"/></a></div>
+					</div>
+				</div> <!-- row -->
+			</div> <!-- container -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
